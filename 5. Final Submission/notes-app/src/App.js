f5e35d7a1016ed490ddc5776c48fd59df1750205
unknown <aditnegarabisnis@gmail.com>
2022-07-11 16:33:20 +0700

    Finsih Desktop noteList layouting

---
+++ 5. Final Submission/notes-app/src/App.js
@@ -0,0 +1,12 @@
+import './global.scss';
+import NoteList from './components/molecules/NoteList/NoteList';
+ 
+const App = () =>{
+  return(
+    <div>
+      <NoteList/>
+    </div>
+  )
+}
+
+export default App;
