67435c760ae51bd89d22a17584ceb487661fe5c4
unknown <aditnegarabisnis@gmail.com>
2022-07-18 14:27:58 +0700

    Finish Close Modal Button

--- 5. Final Submission/notes-app/src/components/molecules/Modal/Modal.js
+++ 5. Final Submission/notes-app/src/components/molecules/Modal/Modal.js
@@ -3,6 +3,7 @@ import Overlay from "../../atoms/Overlay/Overlay";
 import uiStore from "../../../store/uiStore";
 import styles from './Modal.module.scss';
 import CreateForm from '../CreateForm/CreateForm';
+import closeIcon from '../../../assets/icons/close.png';
 
 const Modal = () =>{
     const isCreateModalOpen = uiStore(state => state.isCreateModalOpen);
@@ -10,13 +11,18 @@ const Modal = () =>{
 
     const stopPropagation = (e) =>{
         e.stopPropagation();
-        console.log('Clicking inside modal');
     }
     const renderModal = (isCreateModalOpen) =>{
         return (isCreateModalOpen) ? 
             ( 
             <Overlay onHandleOverlayClicked={toggleCreateModalOpen}>
                 <div className={styles['modal']} onClick={stopPropagation}>
+                    <div className={styles['close-modal__button']}>
+                    <button onClick={toggleCreateModalOpen} aria-label='close modal' >
+                        <img alt='close' src={closeIcon}/>
+                    </button>
+                    </div>
+              
                    <div className={styles['modal-content']} >
                         <CreateForm/>
                    </div>
